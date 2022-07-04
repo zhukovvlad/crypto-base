bbb86400fcea61d550a9dd7f1e87fb3c8c7aee06
zhukovvlad <zhukovvlad@yandex.ru>
2022-07-04 19:08:26 +0300

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,23 +1,3 @@
-// import firebase from "firebase/app";
-// 
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-// 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCdWkx8z5twuAQslcJ8dtIDQiBxMDNXkfI",
-//   authDomain: "crypto-base-db-7e255.firebaseapp.com",
-//   projectId: "crypto-base-db-7e255",
-//   storageBucket: "crypto-base-db-7e255.appspot.com",
-//   messagingSenderId: "848261410541",
-//   appId: "1:848261410541:web:2c8ba2ef9230dd795026e4"
-// };
-// 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
