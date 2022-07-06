0269490ee5472eac3d3da62875ec2dc1867f4c70
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-06 21:59:21 +0300

    Added container to inner page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <SearchAppBar />
-        <AppRouter />
+          <AppRouter />
       </BrowserRouter>
     </div>
   );
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { auth } from "../firebase/firebase.utils";
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState } from "react-firebase-hooks/auth";
+import Container from "@mui/material/Container";
 
 const Chat = () => {
-    const [user] = useAuthState(auth);
-  return <div>Hello, {user.displayName}</div>;
+  const [user] = useAuthState(auth);
+  return (
+    <Container
+      sx={{
+        marginY: 2,
+      }}
+    >
+      Hello, {user.displayName}
+    </Container>
+  );
 };
 
 export default Chat;
