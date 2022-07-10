b25c7e859f2405f6c83a77fceb63d0606dc146be
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-10 16:53:41 +0300

    Added POST function that adds coin to firestore

--- src/components/AddCoin.js
+++ src/components/AddCoin.js
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
 import { TextField, Button, Grid } from "@mui/material";
 import axios from "axios";
-import { HistoricalChart, CoinData } from "../config/api";
+import { CoinData } from "../config/api";
 import findMaxInArray from "../utils/findMax";
+import { db, auth } from "../firebase/firebase.utils";
+import { addDoc, collection } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const AddCoin = () => {
+  const [user] = useAuthState(auth);
   const [coin, setCoin] = useState("");
   const [coinData, setCoinData] = useState([]);
 
-  const fetchHistoricalData = async (coinId) => {
-    const { data } = await axios
-      .get(CoinData(coinId))
-      .catch(error => {
-        // handle error
-        return { data: error };
+  const postCoin = async (entity) => {
+    try {
+      const docRef = await addDoc(collection(db, "coins"), {
+        user: user.uid,
+        coin: entity,
+        athChange: coinData.data.market_data.ath_change_percentage.usd,
       });
+    } catch (error) {
+      console.log("Error adding document: ", error);
+    }
+  };
 
-    // console.log(data.prices ? data.prices: data.response.data);
+  const fetchHistoricalData = async (coinId) => {
+    const data = await axios.get(CoinData(coinId)).catch((error) => {
+      // handle error
+      //return { data: error };
+      return error;
+    });
+
+    console.log(data);
 
     setCoinData(data);
+    if (data) {
+      postCoin(coinId);
+    }
 
     //setCoinData(data.prices ? findMaxInArray(data.prices): data.response.data);
   };
@@ -32,7 +50,6 @@ const AddCoin = () => {
         <Button onClick={() => fetchHistoricalData(coin)}>Add</Button>
       </Grid>
       <Grid item xs={7}></Grid>
-
     </Grid>
   );
 };
