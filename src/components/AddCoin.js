efd8f4373352dcccdda025e7ce59f64aa00753b8
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-10 19:06:14 +0300

    Added useEffect for execution POST function into firebase

--- src/components/AddCoin.js
+++ src/components/AddCoin.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { TextField, Button, Grid } from "@mui/material";
 import axios from "axios";
 import { CoinData } from "../config/api";
@@ -6,23 +6,53 @@ import findMaxInArray from "../utils/findMax";
 import { db, auth } from "../firebase/firebase.utils";
 import { addDoc, collection } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { COIN_DATABASE } from "../utils/consts";
 
 const AddCoin = () => {
   const [user] = useAuthState(auth);
   const [coin, setCoin] = useState("");
   const [coinData, setCoinData] = useState([]);
 
-  const postCoin = async (entity) => {
-    try {
-      const docRef = await addDoc(collection(db, "coins"), {
-        user: user.uid,
-        coin: entity,
-        athChange: coinData.data.market_data.ath_change_percentage.usd,
-      });
-    } catch (error) {
-      console.log("Error adding document: ", error);
+  // const checkIsCoinInDatabase = async ()
+
+  //const postCoin = async (entity) => {
+  //  try {
+  //    const docRef = await addDoc(collection(db, COIN_DATABASE), {
+  //      user: user.uid,
+  //      coin: entity,
+  //      athChange: coinData.data.market_data.ath_change_percentage.usd,
+  //    }, {merge: true});
+  //  } catch (error) {
+  //    console.log("Error adding document: ", error);
+  //  }
+  //};
+
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
     }
-  };
+
+    const postCoin = async (entity) => {
+      try {
+        const docRef = await addDoc(
+          collection(db, COIN_DATABASE),
+          {
+            user: user.uid,
+            coin: entity,
+            athChange: coinData.data.market_data.ath_change_percentage.usd,
+          },
+          { merge: true }
+        );
+      } catch (error) {
+        console.log("Error adding document: ", error);
+      }
+    };
+
+    postCoin(coinData.data.id);
+  }, [coinData, user.uid]);
 
   const fetchHistoricalData = async (coinId) => {
     const data = await axios.get(CoinData(coinId)).catch((error) => {
@@ -34,9 +64,9 @@ const AddCoin = () => {
     console.log(data);
 
     setCoinData(data);
-    if (data) {
-      postCoin(coinId);
-    }
+    //if (data) {
+    //  postCoin(coinId);
+    //}
 
     //setCoinData(data.prices ? findMaxInArray(data.prices): data.response.data);
   };
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,3 +1,5 @@
 export const LOGIN_ROUTE = "/login";
 export const DASHBOARD_ROUTE = "/dashboard";
 export const ADDCOIN_ROUTE = "/addcoin";
+
+export const COIN_DATABASE = "coins";
