43bff471c882c4ff5d3dc093e37dfb132f206764
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-11 22:13:43 +0300

    Fixed some errors in post request

--- src/components/AddCoin.js
+++ src/components/AddCoin.js
@@ -2,32 +2,30 @@ import React, { useEffect, useState, useRef } from "react";
 import { TextField, Button, Grid } from "@mui/material";
 import axios from "axios";
 import { CoinData } from "../config/api";
-import findMaxInArray from "../utils/findMax";
 import { db, auth } from "../firebase/firebase.utils";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDoc, getDocs } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { COIN_DATABASE } from "../utils/consts";
+import { query, where } from "firebase/firestore";
 
 const AddCoin = () => {
   const [user] = useAuthState(auth);
   const [coin, setCoin] = useState("");
   const [coinData, setCoinData] = useState([]);
 
-  // const checkIsCoinInDatabase = async ()
-
-  //const postCoin = async (entity) => {
-  //  try {
-  //    const docRef = await addDoc(collection(db, COIN_DATABASE), {
-  //      user: user.uid,
-  //      coin: entity,
-  //      athChange: coinData.data.market_data.ath_change_percentage.usd,
-  //    }, {merge: true});
-  //  } catch (error) {
-  //    console.log("Error adding document: ", error);
-  //  }
-  //};
-
   const isFirstRender = useRef(true);
+
+  const coinsRef = collection(db, "coins");
+
+  const getCoinFromDatabase = async (coinId) => {
+    const q = query(coinsRef, where("coin", "==", coinId));
+
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data());
+    });
+  };
 
   useEffect(() => {
     if (isFirstRender.current) {
@@ -37,38 +35,33 @@ const AddCoin = () => {
 
     const postCoin = async (entity) => {
       try {
-        const docRef = await addDoc(
-          collection(db, COIN_DATABASE),
-          {
-            user: user.uid,
-            coin: entity,
-            athChange: coinData.data.market_data.ath_change_percentage.usd,
-          },
-          { merge: true }
-        );
+        const docRef = await addDoc(collection(db, COIN_DATABASE), {
+          user: user.uid,
+          coin: entity,
+          athChange: coinData.data.market_data.ath_change_percentage.usd,
+        });
       } catch (error) {
         console.log("Error adding document: ", error);
       }
     };
+
+    if (!coinData?.data?.id) {
+      return;
+    }
 
     postCoin(coinData.data.id);
   }, [coinData, user.uid]);
 
   const fetchHistoricalData = async (coinId) => {
     const data = await axios.get(CoinData(coinId)).catch((error) => {
-      // handle error
-      //return { data: error };
       return error;
     });
 
     console.log(data);
 
     setCoinData(data);
-    //if (data) {
-    //  postCoin(coinId);
-    //}
 
-    //setCoinData(data.prices ? findMaxInArray(data.prices): data.response.data);
+    getCoinFromDatabase(coinId);
   };
 
   return (
