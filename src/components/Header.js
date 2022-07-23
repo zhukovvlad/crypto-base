c06173a60b636a5a5d6759d19bc52a042ef7e7b2
Zhukovvlad <zhukovvlad@yandex.ru>
2022-07-23 14:32:30 +0300

    Changed name App in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,7 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            Crypto Change Monitor
           </Typography>
           {user ? (
             <Button onClick={() => auth.signOut()} color="inherit" variant="outlined">
